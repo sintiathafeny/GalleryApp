8929a7fc782964ed8bb247b090bcdb8f905053e7
Sintia Thafeny <sintiathafeny@outlook.com>
2022-03-25 16:57:05 -0300

    inclusão das rotas

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import {AppLoading} from 'expo';
 export default function App() {
+
+
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      StatusBar style='light' backgroundColor='#000' translucent= {false};
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
